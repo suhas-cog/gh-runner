bb917de9907c784f751ded03f2b312866e28429a
G <2302372@cognizant.com>
2024-09-26 12:21:25 +0530

    latest:1

---
+++ startup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo "Starting supervisor (Docker)"
+sudo service docker start
+
+if [ -n "${GITHUB_REPOSITORY}" ]; then
+  auth_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
+  registration_url="https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY}"
+else
+  auth_url="https://api.github.com/orgs/${GITHUB_OWNER}/actions/runners/registration-token"
+  registration_url="https://github.com/${GITHUB_OWNER}"
+fi
+
+generate_token() {
+  local payload
+  local runner_token
+
+  payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PERSONAL_TOKEN}" "${auth_url}")
+  runner_token=$(echo "${payload}" | jq .token --raw-output)
+
+  if [ "${runner_token}" == "null" ]; then
+    echo "${payload}"
+    exit 1
+  fi
+
+  echo "${runner_token}"
+}
+
+remove_runner() {
+  ./config.sh remove --unattended --token "$(generate_token)"
+}
+
+service docker status
+runner_id=${RUNNER_NAME}_$(openssl rand -hex 6)
+echo "Registering runner ${runner_id}"
+
+RUNNER_TOKEN=$(generate_token)
+test $? -ne 0 && {
+  echo "Debugging token"
+  echo "${RUNNER_TOKEN}"
+  exit 1
+}
+
+./config.sh \
+  --name "${runner_id}" \
+  --labels "${RUNNER_LABELS}" \
+  --token "${RUNNER_TOKEN}" \
+  --url "${registration_url}" \
+  --unattended \
+  --replace \
+  --ephemeral
+
+trap 'remove_runner; exit 130' SIGINT
+trap 'remove_runner; exit 143' SIGTERM
+
+./run.sh "$*"
